f612160c556e0eedc3fee0b170f38a3871a1e173
prostomarusya <93661159+prostomarusya@users.noreply.github.com>
2021-11-05 19:47:29 +0300

    Add files via upload

---
+++ lab4/task 1/LR4T1.cpp
@@ -0,0 +1,54 @@
+﻿//Лабороторная 4, задача 1. Выполнена: Михалевич М.П. Вариант 13
+/*Определить кол-во элементов, значение которых больше среднего значения*/
+
+#include <iostream>
+
+int input() {
+    while (true) {
+        int value;
+
+        std::cout << "Введите размер массива: ";
+        std::cin >> value;
+
+        if (std::cin.fail() || value < 1) {
+            std::cin.clear();
+            std::cin.ignore(32767, '\n');
+            std::cout << "Error\n";
+        }
+        else {
+            std::cin.ignore(32767, '\n');
+
+            return value;
+        }
+    }
+}
+
+int main(){
+
+    setlocale(LC_ALL, "ru");
+    srand(time(NULL));
+    
+    const int  N = 100;
+    int array[N], k, sum = 0, n = 0;
+
+    k = input();
+
+    for (int i = 0; i < k; i++) {
+        array[i] = rand() % 100;
+        sum += array[i];
+
+        std::cout << array[i] << " ";
+    }
+
+    sum = sum / k;
+
+    for (int i = 0; i < k; i++) {
+        if (array[i] > sum) {
+            n++;
+        }
+    }
+
+    std::cout << "\nКол-во элементов, значение которых больше среднего значения: " << n;
+
+    return 0;
+}
