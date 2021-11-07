410e0d20d828d7cd6cb128e6961ad0bfdf3042cc
prostomarusya <93661159+prostomarusya@users.noreply.github.com>
2021-11-07 12:02:21 +0300

    Add files via upload

---
+++ lab4/task 6/LR4T6.cpp
@@ -0,0 +1,63 @@
+﻿//Лабороторная 4, задача 6. Выполнена: Михалевич М.П.
+
+#include <iostream>
+
+int input() {
+    while (true) {
+        int value;
+
+        std::cin >> value;
+
+        if (std::cin.fail() || value < 1 || value > 10) {
+            std::cin.clear();
+            std::cin.ignore(32767, '\n');
+            std::cout << "Error\n";
+        }
+        else {
+            std::cin.ignore(32767, '\n');
+
+            return value;
+        }
+    }
+}
+
+int main(){
+
+    setlocale(LC_ALL, "ru");
+    srand(time(NULL));
+
+    const int N = 10, M = 10;
+    int n, m, sum = 0, max, array[N][M];
+
+    std::cout << "Введите кол-во строк до 10: ";
+    n = input();
+
+    std::cout << "Введите кол-во столбцов до 10: ";
+    m = input();
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+
+            array[i][j] = 1 + rand() & 10;
+            std::cout << array[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl << "Новая матрица: " << std::endl;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+
+            if ((j != 0 && array[i][j-1] > array[i][j]) || ( i != 0 && array[i-1][j] > array[i][j])) {
+                max = (array[i - 1][j] > array[i][j - 1]) ? array[i - 1][j] : array[i][j - 1];
+                array[i][j] = max;
+            }
+
+            std::cout << array[i][j] << " ";
+        }
+       std::cout << std::endl;
+    }
+
+    return 0;
+}
